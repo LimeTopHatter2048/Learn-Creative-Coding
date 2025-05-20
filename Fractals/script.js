ccd48ee344c71c7824c3dc46b580c6a31e8ddd55
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-05-21 09:40:19 +1200

    Fractals-test1

--- Fractals/script.js
+++ Fractals/script.js
@@ -1,8 +1,33 @@
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth * 0.8;
+    canvas.height = window.innerHeight * 0.8;
 
-    
+    // canvas settings
+    ctx.fillStyle = 'green';
+    ctx.strokeStyle = 'yellow';
+    ctx.lineWidth = 30;
+    ctx.lineCap = 'round';
+
+    // effect settings
+    let size = 200;
+
+    ctx.save();
+    ctx.strokeStyle = 'red';
+    ctx.translate(100,100);
+    ctx.scale(0.8, 0.8);
+    ctx.rotate(-0.4);
+    ctx.rotate(0.6);
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    ctx.beginPath();
+    ctx.moveTo(canvas.width/2,canvas.height/2);
+    ctx.lineTo(size, canvas.height/2);
+    ctx.stroke();
+
+    ctx.restore();
+    ctx.beginPath();
+    ctx.moveTo(canvas.width/2,canvas.height/2);
+    ctx.lineTo(size, canvas.height/2);
+    ctx.stroke();
 });
